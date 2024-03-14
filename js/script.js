5d634285ca0a1e25a3488d4906465acad84008a0
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-14 12:15:22 +0100

    start ex 2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-const numGen = [];
-let numUser = parseInt(prompt("inserisci un numero: "));
+// const numGen = [];
 
-let somma = 0
-while(somma < 50) {
-    let numUser = parseInt(prompt("inserisci un numero: "));
-    if (!NaN(numUser)){
-        somma += numUser;
-        numGen.push(numUser);
-    }
+// let somma = 0
+// while(somma < 50) {
+//     let numUser = parseInt(prompt("inserisci un numero: "));
+//     if (!NaN(numUser)){
+//         somma += numUser;
+//         numGen.push(numUser);
+//     }
+// }
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
+ const arrayNumb = parseInt(prompt("Inserisci il numero di array da generare: "));
+
+
+function casualArrGen(n) {
+    const array = [];
+    for (let i = 0; i < n; i++) {
+      array.push(getRandomInteger(0, 10));
+    }
+    return array;
+  }
+  
+ 
+  
+  
